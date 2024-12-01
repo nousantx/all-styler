d44eab2cab1b1958490096431a1475608389a547
nousantx <nousantx@gmail.com>
2024-12-01 18:16:54 +0700

    Add better types

---
+++ src/types/index.ts
@@ -0,0 +1,15 @@
+import type { Property, Values, Classes, Aliases, Breakpoint } from '@tenoxui/core/full'
+import type { ColorOptions, ColorInput } from '@nousantx/color-generator'
+export interface Config {
+  property?: Property
+  coloredProperty?: Property
+  values?: Values
+  classes?: Classes
+  aliases?: Aliases
+  breakpoints?: Breakpoint[]
+  color?: ColorInput
+  colorOption?: ColorOptions
+  attributify?: boolean
+  attributifyPrefix?: string
+  attributifyIgnore?: string[]
+}
