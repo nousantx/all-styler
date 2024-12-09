0fe0cb0c4165022efdddad9a97569e50f93d3ba7
nousantx <nousantx@gmail.com>
2024-12-09 09:39:48 +0700

    Add initial properties from @tenoxui/property

--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,23 @@
 import { MakeTenoxUI } from '@tenoxui/core/full'
+import { property as txProps } from '@tenoxui/property'
 import { merge, createProperty } from '@nousantx/someutils'
 import { generateColors } from '@nousantx/color-generator'
 import type { CoreConfig, Property, Values } from '@tenoxui/core/full'
 import { Config } from './types'
 
-export function createConfig(config: Config): CoreConfig {
-  const {
-    property = {},
-    coloredProperty = {
-      bg: 'backgroundColor',
-      text: 'color'
-    },
-    values = {},
-    classes = {},
-    aliases = {},
-    breakpoints = [],
-    color = { red: '#ef3737' },
-    colorOption = { format: 'object2', output: 'rgb-only' },
-    attributify = true,
-    tenoxuiOption = {}
-  } = config
-
+export function createConfig({
+  property = {},
+  coloredProperty = { 'use-tenoxui': '--colored-feature-tenoxui' },
+  values = {},
+  classes = {},
+  aliases = {},
+  breakpoints = [],
+  color = { red: '#ef3737' },
+  colorOption = { format: 'object2', output: 'rgb-only' },
+  attributify = true,
+  attributifyPrefix = 'nsx-',
+  tenoxuiOption = {}
+}: Config): CoreConfig {
   const colors = generateColors({
     option: colorOption,
     color
@@ -28,24 +25,37 @@ export function createConfig(config: Config): CoreConfig {
 
   return {
     property: {
+      // Added tenoxui default properties
+      ...txProps,
       ...property,
+      // Put the coloredProperty in the end to prioritize this
       ...(createProperty(
         coloredProperty as Record<string, string>,
         'rgb({0} / var(--{1}-opacity, 1))'
-      ) as Property)
+      ) as Property),
+      ...(Object.keys(coloredProperty).reduce<Record<string, string>>((acc, item) => {
+        acc[`${item}-opacity`] = `--${item}-opacity`
+        return acc
+      }, {}) as Property)
     },
     values: merge(colors, values) as Values,
     classes,
     aliases,
     breakpoints,
     attributify,
+    attributifyPrefix,
     ...tenoxuiOption
   }
 }
 
-export function init(config: CoreConfig, selectors: string = '*') {
+export function init(
+  config: CoreConfig,
+  selectors: string = '*',
+  engine: typeof MakeTenoxUI = MakeTenoxUI
+) {
+  console.log(config)
   document.querySelectorAll(selectors).forEach((element) => {
-    new MakeTenoxUI({ element: element as HTMLElement, ...config }).useDOM()
+    new engine({ element: element as HTMLElement, ...config }).useDOM()
   })
 }
 
