0df3405e227b76e9154ffaf4ea4c8efb6fabf811
nousantx <nousantx@gmail.com>
2024-12-09 20:22:15 +0700

    Add utilityClasses option

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,11 +1,27 @@
-import type { CoreConfig, Property, Values, Classes, Aliases, Breakpoint } from '@tenoxui/core/full'
+import type {
+  CoreConfig,
+  Property,
+  CSSPropertyOrVariable,
+  GetCSSProperty,
+  Values,
+  Classes,
+  Aliases,
+  Breakpoint
+} from '@tenoxui/core/full'
 import type { ColorOptions, ColorInput } from '@nousantx/color-generator'
+
+export type UtilityClasses = {
+  [className: string]: {
+    [property in CSSPropertyOrVariable]?: string
+  }
+}
 
 export interface Config {
   property?: Property
-  coloredProperty?: Property
+  coloredProperty?: Record<string, GetCSSProperty>
   values?: Values
   classes?: Classes
+  utilityClasses?: UtilityClasses
   aliases?: Aliases
   breakpoints?: Breakpoint[]
   color?: ColorInput
