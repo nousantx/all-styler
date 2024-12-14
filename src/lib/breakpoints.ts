daa641a57e8f0f4b61064aee8e9c8e8e7e3b0a92
nousantx <nousantx@gmail.com>
2024-12-14 17:25:43 +0700

    Add default breakpoints

---
+++ src/lib/breakpoints.ts
@@ -0,0 +1,16 @@
+import type {Breakpoint} from "@tenoxui/core/full"
+
+
+
+export const defaultBreakpoints: Breakpoint [] = [
+  { name: 'max-sm', max: 640 },
+  { name: 'sm', min: 640 },
+  { name: 'max-md', max: 767.9 },
+  { name: 'md', min: 768 },
+  { name: 'max-lg', max: 1023.9 },
+  { name: 'lg', min: 1024 },
+  { name: 'max-xl', max: 1279.9 },
+  { name: 'xl', min: 1280 },
+  { name: 'max-2xl', max: 1535.9 },
+  { name: '2xl', min: 1536 }
+]
