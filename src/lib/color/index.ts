727f8d310d80cb4b36823bd66dcb50876d4c7399
nousantx <nousantx@gmail.com>
2024-12-18 19:09:36 +0700

    Feat: add custom color geberation

--- src/lib/color/index.ts
+++ src/lib/color/index.ts
@@ -1,8 +1,14 @@
-export const colors: Record<string, string> = {
-  primary: '#ccf654',
-  red: '#f03838',
-  green: '#15e05f',
-  blue: '#3d82f2',
-  yellow: '#f1c230',
-  neutral: '#737373'
+export type ColorScheme = 'light' | 'dark' | 'DEFAULT'
+
+export const colors: Record<ColorScheme, Record<string, string>> = {
+  DEFAULT: {
+    primary: '#ccf654',
+    red: '#f03838',
+    green: '#15e05f',
+    blue: '#3d82f2',
+    yellow: '#f1c230',
+    neutral: '#737373'
+  },
+  light: {},
+  dark: {}
 }
