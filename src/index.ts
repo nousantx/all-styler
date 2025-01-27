996334c3d9e7429258d4bd1e807cef77f9124057
nousantx <nousantx@gmail.com>
2025-01-27 07:03:17 +0700

    feat: use `c-` instead of `fc-` for fixed color prefix

--- src/index.ts
+++ src/index.ts
@@ -9,98 +9,98 @@ import { colors as defaultColors, ColorScheme } from './lib/color'
 import { Config, MainOption } from './types'
 
 function addBaseColors(colors: Record<string, string>): Record<string, string> {
-	const result = { ...colors }
-	Object.entries(colors).forEach(([key, value]) => {
-		if (key.endsWith('-500')) {
-			const baseColorName = key.replace('fc-', '').replace('-500', '')
-			result[baseColorName] = value
-		}
-	})
-	return result
+  const result = { ...colors }
+  Object.entries(colors).forEach(([key, value]) => {
+    if (key.endsWith('-500')) {
+      const baseColorName = key.replace('fc-', '').replace('-500', '')
+      result[baseColorName] = value
+    }
+  })
+  return result
 }
 
 export function createConfig(options: Config = {}): CoreConfig {
-	const {
-		shorthand = { 'nsx-tenoxui-template': '--ignore-this' },
-		coloredShorthand = { 'use-tenoxui': '--colored-feature-tenoxui' },
-		valueAlias = {},
-		utilityFirst = {},
-		utilityClass = {},
-		alias = {},
-		breakpoint = [],
-		color = {},
-		isDark = false,
-		attributify = true,
-		attributifyPrefix = 'nsx-',
-		plugins = [],
-		tenoxuiOption = {}
-	} = options
+  const {
+    shorthand = { 'nsx-tenoxui-template': '--ignore-this' },
+    coloredShorthand = { 'use-tenoxui': '--colored-feature-tenoxui' },
+    valueAlias = {},
+    utilityFirst = {},
+    utilityClass = {},
+    alias = {},
+    breakpoint = [],
+    color = {},
+    isDark = false,
+    attributify = true,
+    attributifyPrefix = 'nsx-',
+    plugins = [],
+    tenoxuiOption = {}
+  } = options
 
-	const colorScheme: ColorScheme = isDark ? 'dark' : 'light'
-	const inputColors: Record<string, string> = {
-		...defaultColors.DEFAULT,
-		...color.DEFAULT
-	}
+  const colorScheme: ColorScheme = isDark ? 'dark' : 'light'
+  const inputColors: Record<string, string> = {
+    ...defaultColors.DEFAULT,
+    ...color.DEFAULT
+  }
 
-	// generate color aliases for tenoxui
-	const generatedColors = generateColors({
-		color: { ...inputColors, ...color[colorScheme] },
-		isDark
-	}) as Values
+  // generate color aliases for tenoxui
+  const generatedColors = generateColors({
+    color: { ...inputColors, ...color[colorScheme] },
+    isDark
+  }) as Values
 
-	const fixedColorsRaw = generateColors({
-		color: inputColors,
-		prefix: 'fc-'
-	})
+  const fixedColorsRaw = generateColors({
+    color: inputColors,
+    prefix: 'c-'
+  })
 
-	// Add base color names to fixedColors
-	const fixedColors = addBaseColors(fixedColorsRaw) as Values
+  // Add base color names to fixedColors
+  const fixedColors = addBaseColors(fixedColorsRaw) as Values
 
-	// added custom plugins field
-	const usedPlugins = plugins.reduce((acc, plugin) => merge(acc, plugin), {})
+  // added custom plugins field
+  const usedPlugins = plugins.reduce((acc, plugin) => merge(acc, plugin), {})
 
-	return merge(
-		{
-			property: createProperties(shorthand, coloredShorthand),
-			values: merge(generatedColors, fixedColors, valueAlias),
-			classes: merge(defaultClasses, transformClasses(utilityClass), utilityFirst) as Classes,
-			aliases: alias,
-			breakpoints: [...defaultBreakpoints, ...breakpoint],
-			attributify,
-			attributifyPrefix,
-			...tenoxuiOption
-		},
-		usedPlugins
-	)
+  return merge(
+    {
+      property: createProperties(shorthand, coloredShorthand),
+      values: merge(generatedColors, fixedColors, valueAlias),
+      classes: merge(defaultClasses, transformClasses(utilityClass), utilityFirst) as Classes,
+      aliases: alias,
+      breakpoints: [...defaultBreakpoints, ...breakpoint],
+      attributify,
+      attributifyPrefix,
+      ...tenoxuiOption
+    },
+    usedPlugins
+  )
 }
 
 export function init(options: MainOption) {
-	const {
-		config = {},
-		root = document,
-		selectors = '*',
-		useDOM = true,
-		engine = MakeTenoxUI
-	} = options
+  const {
+    config = {},
+    root = document,
+    selectors = '*',
+    useDOM = true,
+    engine = MakeTenoxUI
+  } = options
 
-	root.querySelectorAll(selectors).forEach(element => {
-		// create tenoxui instance
-		const styler = new engine({ element: element as HTMLElement, ...config })
+  root.querySelectorAll(selectors).forEach((element) => {
+    // create tenoxui instance
+    const styler = new engine({ element: element as HTMLElement, ...config })
 
-		// if the MutationObserver is available, use this instead
-		if (useDOM) styler.useDOM()
-		// using class names scan method without MutationObserver
-		else element.classList.forEach(className => styler.applyStyles(className))
-	})
+    // if the MutationObserver is available, use this instead
+    if (useDOM) styler.useDOM()
+    // using class names scan method without MutationObserver
+    else element.classList.forEach((className) => styler.applyStyles(className))
+  })
 }
 
 export { Config } from './types'
 export { MakeTenoxUI } from '@tenoxui/core/full'
 export type { CoreConfig, Property, Values, Classes, Aliases, Breakpoint } from '@tenoxui/core/full'
 export {
-	merge,
-	transformClasses,
-	createValue,
-	createSameValue,
-	createProperty
+  merge,
+  transformClasses,
+  createValue,
+  createSameValue,
+  createProperty
 } from '@nousantx/someutils'
